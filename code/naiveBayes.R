69113f07310a71f1a271f2e692abefb5bd7d84af
Yishu Xue <xysfyq@163.com>
2018-03-05 23:23:49 -0500

    naive bayes working

--- code/naiveBayes.R
+++ code/naiveBayes.R
@@ -1,4 +1,4 @@
-pkgs <- c("tm", "e1071", "gmodels")
+pkgs <- c("tm", "e1071", "gmodels", "stringr", "SnowballC", "wordcloud", "caret")
 for(i in 1:length(pkgs)){
     if (! pkgs[i] %in% installed.packages()){
         install.packages(pkgs[i], repos = "https://cloud.r-project.org/")
@@ -6,68 +6,77 @@ for(i in 1:length(pkgs)){
 }
 
 library(tm)
+library(stringr)
 library(gmodels)
 library(e1071)
 library(SnowballC)
 library(wordcloud)
+library(ggplot2)
+library(caret)
 sms <- read.csv("../spam.csv", stringsAsFactors = FALSE, encoding = "latin1")[,1:2]
 sms$v1 <- factor(sms$v1)
 names(sms) <- c("Label", "Text")
 
-str(sms)
-
-## data preparation
-sms_corpus <- VCorpus(VectorSource(sms$Text))
-print(sms_corpus)
+ggplot(sms, aes(x = Label, fill = Label)) + geom_bar(stat = "count")
+sms$Length <- str_length(sms$Text)
+ggplot(sms, aes(x = Length, fill = Label)) + geom_histogram(binwidth = 5)
 
 ## clean redundant stuff
-corpus_clean <- tm_map(sms_corpus, content_transformer(tolower))
-corpus_clean <- tm_map(corpus_clean, removeNumbers)
-corpus_clean <- tm_map(corpus_clean, removeWords, stopwords())
-corpus_clean <- tm_map(corpus_clean, removePunctuation)
-corpus_clean <- tm_map(corpus_clean, stripWhitespace)
+## data preparation
+smsCorpus <- VCorpus(VectorSource(sms$Text))
+
+corpusClean <- tm_map(smsCorpus, removeWords, stopwords())
+corpusClean <- tm_map(corpusClean, stripWhitespace)
+corpusClean <- tm_map(smsCorpus, content_transformer(tolower))
+corpusClean <- tm_map(corpusClean, removeNumbers)
+corpusClean <- tm_map(corpusClean, removePunctuation)
+corpusClean <- tm_map(corpusClean, stemDocument)
 
 ## 
-corpus_clean <- Corpus(VectorSource(corpus_clean))
-dtm <- DocumentTermMatrix(corpus_clean)
-
+dtm <- DocumentTermMatrix(corpusClean)
 
 ## split training and testing
+set.seed(1234)
+indHam <- sample(which(sms$Label == "ham"), 0.8 * sum(sms$Label == "ham"), 
+                                     replace = FALSE)
+set.seed(5678)
+indSpam <- sample(which(sms$Label == "spam"), 0.8 * sum(sms$Label == "spam"), 
+                                     replace = FALSE)
+indTrain <- c(indHam, indSpam)
+indTest <- setdiff(1:nrow(sms), indTrain)
 
-datTrain <- sms[1:4200, ]
-datTest <- sms[4201:5572, ]
+datTrain <- sms[indTrain, ]
+datTest <- sms[indTest, ]
+corpusTrain <- corpusClean[indTrain]
+corpusTest <- corpusClean[indTest]
 
 ## labels
-labelTrain <- sms$Label[1:4200]
-labelTest <- sms$Label[4201:5572]
+labelTrain <- sms$Label[indTrain]
+labelTest <- sms$Label[indTest]
 
 ## split the document-term matrix
-dtmTrain <- dtm[1:4200, ]
-dtmTest <- dtm[4201:5572, ]
+dtmTrain <- dtm[indTrain,]
+dtmTest <- dtm[indTest, ]
 
-## wordcloud for spam and ham, separately
-spam <- subset(datTrain, Label == "spam")
-ham <- subset(datTest, Label == "ham")
-wordcloud(spam$Text, max.words = 40)
-wordcloud(ham$Text, max.words = 40)
+freq5 <- findFreqTerms(dtmTrain, 5)
+length(freq5)
 
-## frequent words
-freqTerms <- findFreqTerms(dtmTrain, 5)
-reducedDtmTrain <- dtmTrain[, freqTerms]
-reducedDtmTest <-  dtmTest[, freqTerms]
-
-## convert 0, 1 in DTM to yes/no factor, and apply to reduced matrices
+rdtmTrain <- dtmTrain[, freq5]
+rdtmTest <- dtmTest[, freq5]
 
 convertCount <- function(x){
-    x <- factor(x > 0, levels = c(0, 1), labels = c("No", "Yes"))
+    x <- as.factor(ifelse(x > 0, "yes", "no"))
     return(x)
 }
 
-reducedDtmTrain <- apply(reducedDtmTrain, 2, convertCount)
-reducedDtmTest <- apply(reducedDtmTest, 2, convertCount)
+crdtmTrain <- apply(rdtmTrain, 2, convertCount)
+crdtmTest <- apply(rdtmTest, 2, convertCount)
 
-smsClassifier <- naiveBayes(reducedDtmTrain, labelTrain)
-smsPred <- predict(smsClassifier, reducedDtmTest)
+
+smsClassifier <- naiveBayes(crdtmTrain, labelTrain)
+smsPred <- predict(smsClassifier, crdtmTest)
 
 CrossTable(smsPred, datTest$Label, prop.chisq = FALSE, prop.t = FALSE, prop.c = TRUE, 
            prop.r = TRUE,dnn = c("predicted", "actual"))
+
+confusionMatrix(smsPred, datTest$Label)
